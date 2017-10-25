f683bd128e16aa517a82b6771278ab31f6b35b43
Yuri de Paula <yuri.oliveira@democrata.com.br>
2017-10-25 14:16:51 -0200

    bootstrap

--- FrontEnd-controleAtividades/src/app/bread-crumb/bread-crumb.component.ts
+++ FrontEnd-controleAtividades/src/app/bread-crumb/bread-crumb.component.ts
@@ -10,18 +10,24 @@ export class BreadCrumbComponent implements OnInit {
 
   private static breads: any;
   public bread: any;
+  public header: string;
 
   constructor() { }
 
   ngOnInit() {
     //this.bread = BreadCrumbComponent.breads;
-    /*this.bread = [{'nome': 'HOME'      , 'link': '/home'}, 
-    {'nome': 'INTEGRANTE', 'link': '/intregrante'}, 
-    {'nome': 'CADASTRO'  , 'link': '/integrante/novo'}];*/
+    this.bread = [{'nome': 'HOME'      , 'link': '/home'}, 
+    {'nome': 'INTEGRANTE', 'link': '/integrante'}, 
+    {'nome': 'CADASTRO'  , 'link': '/integrante/novo'}];
+    this.header = 'Exemplo de Header'
   }
 
   public setBreads(data: any){
     BreadCrumbComponent.breads = data;
+  }
+
+  public setHeader(data: string){
+    this.header = data;
   }
 
 }
